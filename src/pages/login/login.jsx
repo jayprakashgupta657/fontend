b31d65449f22ebdc40f29fe14046d0771ae2d147
jayprakashgupta657 <jayprakashguptajg657@gmail.com>
2025-07-21 09:54:32 +0545

    first commit

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -205,37 +205,6 @@ const Login = ({ setIsAuthenticated }) => {
             </div>
         </div>
     );
-
-    // return (
-    //     <div className="login-page">
-    //         <h1>Login</h1>
-    //         {error && <p className="error-message">{error}</p>}
-    //         <form onSubmit={handleSubmit}>
-    //             <div>
-    //                 <label>Username:</label>
-    //                 <input
-    //                     type="text"
-    //                     value={username}
-    //                     onChange={(e) => setUsername(e.target.value)}
-    //                     required
-    //                 />
-    //             </div>
-    //             <div>
-    //                 <label>Password:</label>
-    //                 <input
-    //                     type="password"
-    //                     value={password}
-    //                     onChange={(e) => setPassword(e.target.value)}
-    //                     required
-    //                 />
-    //             </div>
-    //             <button type="submit" className="login-button">Login</button>
-    //         </form>
-    //         <p className="signup-text">
-    //             Don't have an account? <Link to="/signup">Sign up</Link>
-    //         </p>
-    //     </div>
-    // );
 };
 
 export default Login;
